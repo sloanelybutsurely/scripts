26320aaf8ee2d29bc2decbd8f1a60a7d1f8085ec
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2024-03-02 15:26:46 -0500

    add options to scripts.new

--- lib/mix/tasks/scripts.new.ex
+++ lib/mix/tasks/scripts.new.ex
@@ -2,18 +2,23 @@ defmodule Mix.Tasks.Scripts.New do
   use Mix.Task
   import Mix.Generator
 
-  @args []
+  @args [deps_only: :boolean, tests: :boolean]
   def run(args) do
     scripts_dir = "./bin"
-    {_, [name]} = OptionParser.parse!(args, strict: @args)
+    {opts, [name]} = OptionParser.parse!(args, strict: @args)
+    opts = Keyword.merge([deps_only: false, tests: false], opts)
 
     mod = Macro.camelize(name)
     script = Macro.underscore(name)
     script_path = Path.join(scripts_dir, script)
 
-    assigns = [mod: mod, script: script]
+    assigns = Keyword.merge([mod: mod, script: script], opts)
 
-    copy_template("templates/script.exs.eex", script_path, assigns)
+    template =
+      EEx.eval_file("templates/script.exs.eex", assigns: assigns)
+      |> Code.format_string!()
+
+    create_file(script_path, template)
     File.chmod!(script_path, 0o755)
   end
 end
