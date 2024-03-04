886f081b0ad635777cf3f4c84461369f5760acb5
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2024-03-04 11:54:08 -0500

    add docs to scripts.new

--- lib/mix/tasks/scripts.new.ex
+++ lib/mix/tasks/scripts.new.ex
@@ -2,6 +2,19 @@ defmodule Mix.Tasks.Scripts.New do
   use Mix.Task
   import Mix.Generator
 
+  @moduledoc """
+  Creates a new executable Elixir script based on the template in `templates/script.exs.eex`.
+
+  ## Options
+
+  | Flag                        |                                                                              | default        |
+  | --------------------------- | ---------------------------------------------------------------------------- | -------------- |
+  | --deps-only, --no-deps-only | includes a block that terminates the script after running `Mix.install(...)` | --no-deps-only |
+  | --tests, --no-tests         | includes a block that runs inline `ExUnit` tests when `MIX_ENV = test`       | --no-tests     |
+
+  """
+  @shortdoc "create a new executable script based on the template"
+
   @args [deps_only: :boolean, tests: :boolean]
   def run(args) do
     scripts_dir = "./bin"
